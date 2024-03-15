e7c5bb6bd46936a941f9d00d4b2fb911cbfd9836
Roman Pavlosiuk <romanpavlosiuk10@gmail.com>
2024-03-15 04:31:17 +0200

    login

--- app.py
+++ app.py
@@ -1,115 +1,126 @@
 '''app.py'''
-from flask import Flask, render_template, request, redirect, jsonify, session
+import secrets
+import re
+from flask import Flask, render_template, request, redirect, jsonify, session, url_for
 from flask.views import MethodView
 from flask_pymongo import PyMongo
-import json
+from werkzeug.security import generate_password_hash, check_password_hash
+
+secret_key = secrets.token_hex(16)
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb+srv://pavlosiukpn:U221Bd9n@cookmatecluster.uqlmdxd.mongodb.net/python_project"
-app.secret_key = 'helloworld+:>"LKHL'
+app.secret_key = secret_key
 
 mongo = PyMongo(app)
+records = mongo.db.users
 
-class MainPage(MethodView):
+class MainView(MethodView):
     '''MainPage'''
     def get(self):
         '''open_main_page'''
         return render_template('main_page.html')
 
 class SearchView(MethodView):
-    '''search ingredients'''
+    '''SearchView'''
     def get(self):
-        '''search ingredients'''
+        '''Search'''
         query = request.args.get('query')
         results = mongo.db.ingredients.find({"name": {"$regex": query.strip()}})
         suggestions = sorted([result['name'] for result in results], key=len)
         return jsonify(suggestions)
 
-class LoginPage(MethodView):
-    '''LoginPage'''
+class LoginView(MethodView):
+    '''Login'''
     def get(self):
-        '''open login'''
-        return render_template('login.html')
+        '''LoginPage'''
+        if 'email' in session:
+            return redirect(url_for('liked'))
+        return render_template('login.html', message='')
 
-class SignUpPage(MethodView):
-    '''SingUpPage'''
+    def post(self):
+        '''Login'''
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user_found = records.find_one({'email': email})
+        if user_found:
+            user_val = user_found['email']
+            passwordcheck = user_found['password']
+            if check_password_hash(passwordcheck, password):
+                session['email'] = user_val
+                return redirect(url_for('liked'))
+            return render_template('login.html', message='Wrong Password!')
+        return render_template('login.html', message='User does not exist!')
+
+class SignUpView(MethodView):
+    '''SignUp'''
     def get(self):
-        '''open sign up'''
-        return render_template('sign_up.html')
+        '''SignUpPage'''
+        if 'email' in session:
+            return redirect(url_for('profile'))
+        return render_template('sign_up.html', message='')
 
-class IngredientsPage(MethodView):
+    def post(self):
+        '''SignUp'''
+        email = request.form.get('email')
+        if validate_email(email):
+            password = request.form.get('password')
+            if not email or not password:
+                return render_template('sign_up.html', message='Please fill in all the fields')
+            if records.find_one({'email': email}):
+                return render_template('sign_up.html', message='User already exists!')
+            hashed = generate_password_hash(password, method='scrypt')
+            user_input = {'email': email, 'password': hashed, 'liked': [], \
+                'rated': {'5':{}, "4":{}, '3':{}, '2':{}, '1':{}, '0':{}}}
+            records.insert_one(user_input)
+            session['email'] = email
+            return redirect(url_for('liked'))
+        return render_template('sign_up.html', message='Wrong email form')
+
+class IngredientsView(MethodView):
     '''Ingredients'''
     def get(self):
         '''open ingredients'''
         return render_template('ingredients.html')
 
-class ProfilePage(MethodView):
+class ProfileView(MethodView):
     '''Profile'''
     def get(self):
         '''open profile'''
         return render_template('profile.html')
 
-class TinderPage(MethodView):
+class TinderView(MethodView):
     '''Tinder'''
     def get(self):
         '''open tinder'''
         return render_template('tinder.html')
 
-class LikedPage(MethodView):
+class LikedView(MethodView):
     '''Liked'''
     def get(self):
         '''open liked'''
         return render_template('liked.html')
 
-class RatedPage(MethodView):
+class RatedView(MethodView):
     '''Rated'''
     def get(self):
         '''open rated'''
         return render_template('rated.html')
 
-class StoreDataView(MethodView):
-    def get(self):
-        data = request.data
-        data = json.loads(data)
-        # Now data is a Python list
-        # Store it in the session
-        print(f"{data=}")
-        session['selected_ingredients'] = data
-        found_recipes = find_with_majority_ingredients(data, 0.5)
-        return '', 200  # Return found_recipes in the response
+def validate_email(email:str):
+    '''email'''
+    return bool(re.fullmatch(r"^(?!\.)[a-z!#$%&'*+\-/=?^_`{|}~]+(?:\.[a-z!#$%&'*+\-/=?^_`"+\
+        r"{|}~]{1,64})*@[a-z]{1,255}(\.(gmail|ucu|com|org|edu|gov|net|ua))+", email))
 
-# class StoreDataView(MethodView):
-#     def post(self):
-#         data = request.data
-#         data = json.loads(data)
-#         print(session)
-#         session['selected_ingredients'] = data
-#         print(session)
-#         found_recipes = find_with_majority_ingredients(data, 0.5)
-#         return jsonify(found_recipes), 200
-
-def find_with_majority_ingredients(ingredient_list, amount: float):
-    all_docs = mongo.db.recipes.find({}, {"Cleaned_Ingredients": 1, "Image_Name": 1, "Title": 1})  # only return the 'cleaned_ingredients' field
-    matching_docs = []
-    print("docs: ", all_docs)
-    for doc in all_docs:
-        doc_ingredients = doc['Cleaned_Ingredients'][2:-2].split("', '")  # assuming ingredients are comma-separated
-        common_ingredients = set(doc_ingredients) & set(ingredient_list)
-        if len(common_ingredients) / len(doc_ingredients) > amount:
-            matching_docs.append(doc['Image_Name'])
-    return matching_docs
-
-app.add_url_rule('/', view_func=MainPage.as_view('main_page'))
-app.add_url_rule('/ingredients', view_func=IngredientsPage.as_view('ingredients'))
-app.add_url_rule('/profile', view_func=ProfilePage.as_view('profile'))
-app.add_url_rule('/login', view_func=LoginPage.as_view('login'))
-app.add_url_rule('/sign_up', view_func=SignUpPage.as_view('sign_up'))
+app.add_url_rule('/', view_func=MainView.as_view('main_page'))
+app.add_url_rule('/ingredients', view_func=IngredientsView.as_view('ingredients'))
+app.add_url_rule('/login', view_func=LoginView.as_view('log_in'))
+app.add_url_rule('/sign_up', view_func=SignUpView.as_view('sign_up'))
 app.add_url_rule('/search', view_func=SearchView.as_view('search_view'))
-app.add_url_rule('/tinder', view_func=TinderPage.as_view('tinder'))
-app.add_url_rule('/store_data', view_func=StoreDataView.as_view('store_data'))
-app.add_url_rule('/rated', view_func=StoreDataView.as_view('rated'))
-app.add_url_rule('/liked', view_func=StoreDataView.as_view('liked'))
+app.add_url_rule('/tinder', view_func=TinderView.as_view('tinder'))
+app.add_url_rule('/rated', view_func=RatedView.as_view('rated'))
+app.add_url_rule('/liked', view_func=LikedView.as_view('liked'))
 
 if __name__ == '__main__':
     app.run(debug=True)
