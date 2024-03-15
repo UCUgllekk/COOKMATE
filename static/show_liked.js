2df199068d1756ef6514319bd62cf63f0813d825
Lev Lysyk <levlisik@gmail.com>
2024-03-15 11:10:02 +0200

    image style

--- static/show_liked.js
+++ static/show_liked.js
@@ -3,12 +3,18 @@ if (liked_recipes) {
     for (var i = 0; i < liked_recipes.length; i++) {
         liked_recipes[i] = liked_recipes[i].slice(1, -1).split('", "');
     }
-    ingredients_section = document.getElementById("ingredients_section");
     console.log(liked_recipes)
     for (var i = 0; i < liked_recipes.length; i ++) {
+        ingredients_section = document.createElement("div");
+        ingredients_section.classList.add("ingredients-section")
+        var titleDiv = document.createElement('div')
+        titleDiv.classList.add("recipe-title")
+        titleDiv.innerText = liked_recipes[i][1]
+
+        ingredients_section.append(titleDiv)
         var column_div = document.createElement('div');
         column_div.classList.add("ingredients-column");
-
+        
         var img = document.createElement('img');
         img.src = "static/Food Images/"+ liked_recipes[i][0] + ".jpg";
         img.loading = "lazy";
@@ -23,12 +29,20 @@ if (liked_recipes) {
             ingredients[k] = " • " + ingredients[k] + "<br />"
         }
         textdiv.innerHTML = ingredients.join("")
-
+        
         div.append(textdiv)
+
+        // var show_recipe = document.createElement("a")
+        // show_recipe.idli
+        // show_recipe.classList.add("find-meal visible")
+        // <div class="find-meal-container">
+        //   <a href="javascript:void(0);" id="expandButton" class="find-meal visible">find a meal</a>
+        // </div>
         
         ingredients_section.append(column_div)
         ingredients_section.append(div)
-
+        
+        document.getElementById("recipe-details").append(ingredients_section)
     }
 }
 console.log(liked_recipes)
