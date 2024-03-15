f74239f028931183c6a81296ca2caa7106a134ee
Roman Pavlosiuk <romanpavlosiuk10@gmail.com>
2024-03-15 21:50:39 +0200

    some changes

--- app.py
+++ app.py
@@ -222,19 +222,20 @@ def find_with_majority_ingredients(ingredient_list, amount: float):
     return matching_docs
 
 
-app.add_url_rule('/', view_func=MainView.as_view('main_page'))
-app.add_url_rule('/ingredients', view_func=IngredientsView.as_view('ingredients'))
-app.add_url_rule('/login', view_func=LoginView.as_view('log_in'))
-app.add_url_rule('/sign_up', view_func=SignUpView.as_view('sign_up'))
-app.add_url_rule('/search', view_func=SearchView.as_view('search_view'))
-app.add_url_rule('/tinder', view_func=TinderView.as_view('tinder'))
-app.add_url_rule('/liked', view_func=LikedView.as_view('liked'))
-app.add_url_rule('/store_data', view_func=StoreDataView.as_view('store_data'))
-app.add_url_rule('/store_liked_recipes', view_func=StoreLikedRecipesView.as_view('store_liked_recipes'))
-app.add_url_rule('/rated', view_func=RatedView.as_view('recipes'))
-app.add_url_rule('/rate', view_func=RateView.as_view('rate'))
-app.add_url_rule('/recipe/<recipe_id>/', view_func=RecipeView.as_view('recipe'))
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app.add_url_rule('/', view_func=MainView.as_view('main_page'))
+    app.add_url_rule('/ingredients', view_func=IngredientsView.as_view('ingredients'))
+    app.add_url_rule('/login', view_func=LoginView.as_view('log_in'))
+    app.add_url_rule('/sign_up', view_func=SignUpView.as_view('sign_up'))
+    app.add_url_rule('/search', view_func=SearchView.as_view('search_view'))
+    app.add_url_rule('/tinder', view_func=TinderView.as_view('tinder'))
+    app.add_url_rule('/liked', view_func=LikedView.as_view('liked'))
+    app.add_url_rule('/store_data', view_func=StoreDataView.as_view('store_data'))
+    app.add_url_rule('/store_liked_recipes', view_func=StoreLikedRecipesView.as_view('store_liked_recipes'))
+    app.add_url_rule('/rated', view_func=RatedView.as_view('recipes'))
+    app.add_url_rule('/rate', view_func=RateView.as_view('rate'))
+    app.add_url_rule('/recipe/<recipe_id>/', view_func=RecipeView.as_view('recipe'))
