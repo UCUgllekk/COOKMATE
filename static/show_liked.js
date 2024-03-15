8733d71fcef93d16f68d45f1c83ee273daa54894
Lev Lysyk <levlisik@gmail.com>
2024-03-15 16:37:15 +0200

    ingredients css and jump to recipe

--- static/show_liked.js
+++ static/show_liked.js
@@ -11,7 +11,7 @@ if (liked_recipes) {
         titleDiv.classList.add("recipe-title")
         titleDiv.innerText = liked_recipes[i][1]
 
-        ingredients_section.append(titleDiv)
+        // ingredients_section.append(titleDiv)
         var column_div = document.createElement('div');
         column_div.classList.add("ingredients-column");
         
@@ -21,7 +21,6 @@ if (liked_recipes) {
         img.classList.add("img-3");
         column_div.append(img);
 
-        var div = document.createElement("div");
         var textdiv = document.createElement("div")
         textdiv.classList.add("ingredient-text")
         ingredients = liked_recipes[i][2].split("; ")
@@ -29,20 +28,64 @@ if (liked_recipes) {
             ingredients[k] = " • " + ingredients[k] + "<br />"
         }
         textdiv.innerHTML = ingredients.join("")
-        
-        div.append(textdiv)
 
-        // var show_recipe = document.createElement("a")
-        // show_recipe.idli
-        // show_recipe.classList.add("find-meal visible")
+        var show_recipe = document.createElement("a")
+        // show_recipe.id = 
+        show_recipe.innerText = "jump to recipe"
+        show_recipe.href = "javascript:void(0);"
+        show_recipe.classList.add("find-meal")
+        show_recipe.classList.add("visible")
+
+        var find_meal_div = document.createElement("div")
+        find_meal_div.classList.add("find-meal-container")
+        find_meal_div.append(show_recipe)
         // <div class="find-meal-container">
         //   <a href="javascript:void(0);" id="expandButton" class="find-meal visible">find a meal</a>
         // </div>
         
         ingredients_section.append(column_div)
-        ingredients_section.append(div)
-        
-        document.getElementById("recipe-details").append(ingredients_section)
+        ingredients_section.append(textdiv)
+
+        document.getElementsByClassName("recipe-details")[0].append(titleDiv)
+        document.getElementsByClassName("recipe-details")[0].append(ingredients_section)
+        document.getElementsByClassName("recipe-details")[0].append(find_meal_div)
     }
 }
 console.log(liked_recipes)
+console.log(document.getElementById("expandButton"))
+const expandButtons = document.getElementsByClassName('find-meal');
+expandButtons_dict = {}
+console.log(expandButtons)
+for (var i = 0; i < expandButtons.length; i ++) {
+    expandButtons_dict[i] = expandButtons[i]
+    expandButtons[i].value = i
+    expandButtons[i].addEventListener('click', function() {
+        const recipeTitle = document.createElement('div');
+        recipeTitle.classList.add('recipe-title2');
+        
+        // console.log(liked_recipes[i][3])
+        console.log(this.value)
+        recipeTitle.innerText = 'Recipe';
+        recipeTitle.style.fontWeight = 'bold';
+        recipeTitle.style.border = 'none'; // Remove the borderf
+        recipeTitle.style.textAlign = 'left'; // Align text to the left
+
+        const recipeDescription = document.createElement('div');
+        recipeDescription.classList.add('recipe-description');
+        recipeDescription.innerHTML = liked_recipes[this.value][3].split(". ").join(".<br \>");
+        const findMealContainers = document.getElementsByClassName('find-meal-container');
+        findMealContainers[this.value].parentNode.insertBefore(recipeTitle, findMealContainers[this.value].nextSibling);
+        findMealContainers[this.value].parentNode.insertBefore(recipeDescription, recipeTitle.nextSibling);
+        recipeTitle.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        // Remove the button after clicking
+        expandButtons_dict[this.value].parentNode.removeChild(expandButtons_dict[this.value]);
+
+
+        // Add CSS styles to ingredient-text class
+        const ingredientText = document.querySelectorAll('.ingredient-text');
+        ingredientText.forEach(item => {
+        item.style.overflowWrap = 'break-word';
+        item.style.listStyleType = 'disc';
+        });
+    });
+}
