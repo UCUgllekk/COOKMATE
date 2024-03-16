90e2d3cfcb2ff2a9d5db1a60aff4203420603409
Roman Pavlosiuk <romanpavlosiuk10@gmail.com>
2024-03-16 18:10:18 +0200

    a

--- static/search.js
+++ static/search.js
@@ -1,39 +1,28 @@
 var selected_ingredients = []
-// Add an event listener for the 'submit' event to the form
 document.getElementById('search-form').addEventListener('submit', function(event) {
-    // Prevent the form from being submitted and the page from reloading
     event.preventDefault();
 
-    // Get all the suggestions
     var suggestionsDiv = document.getElementById('suggestions');
     var suggestions = Array.from(suggestionsDiv.getElementsByTagName('button'));
-
-    // If there are any suggestions
-    if (suggestions.length > 0) {
-        // Find the shortest suggestion
+    console.log()
+    if (suggestions.length > 0 && suggestions[0].textContent != "No such ingredient") {
         var shortestSuggestion = suggestions[0]
 
-        // Set the value of the search bar to the shortest suggestion
         var searchBar = document.getElementById('search-bar');
         searchBar.value = shortestSuggestion.textContent;
 
-        // Get all the currently selected ingredients
         var selectedIngredients = Array.from(document.getElementById('selected-ingredients').getElementsByTagName('div'));
         console.log(shortestSuggestion.textContent)
-        // Check if the selected ingredient is already in the list of selected ingredients
         var alreadySelected = selectedIngredients.some(function(ingredientDiv) {
             return ingredientDiv.textContent === shortestSuggestion.textContent;
         });
 
-        // If the ingredient is not already selected
         if (!alreadySelected) {
-            // Create a new div for the selected ingredient
             var ingredientDiv = document.createElement('div');
             ingredientDiv.textContent = shortestSuggestion.textContent;
             ingredientDiv.classList.add("tag");
             selected_ingredients.push(shortestSuggestion.textContent);
             console.log(selected_ingredients);
-            // Add the new div to the list of selected ingredients
             document.getElementById('selected-ingredients').appendChild(ingredientDiv);
         }
     };
@@ -42,9 +31,7 @@ document.getElementById('search-form').addEventListener('submit', function(event
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Attach the click event listener to the document
     document.addEventListener('click', function(event) {
-        // If the clicked element has the class name "option"
         if (event.target.classList.contains('option')) {
             console.log("option")
             console.log(event.target)
