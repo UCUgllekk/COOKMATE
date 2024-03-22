c6a89e20ff0a26bb0af550bb313f1154cef46dbc
Roman Pavlosiuk <romanpavlosiuk10@gmail.com>
2024-03-22 13:01:15 +0200

    QoL 5

--- user_profile.py
+++ user_profile.py
@@ -15,9 +15,9 @@ class LikedView(MethodView):
             for liked_recipe, data in liked.items():
                 if liked_recipe in user['rated']:
                     new_liked = {'Ingredients': data['Ingredients'],
-                                 'Instructions': data['Instructions'],
-                                 'Image_Name': data['Image_Name'],
-                                 'Rating': user['rated'][liked_recipe]['Rating']}
+                                    'Instructions': data['Instructions'],
+                                    'Image_Name': data['Image_Name'],
+                                    'Rating': user['rated'][liked_recipe]['Rating']}
                     users.update_one({'email': user_email}, {'$set': {f'liked.{liked_recipe}':\
                         new_liked}})
             return render_template('liked.html', recipes = liked)
@@ -41,12 +41,12 @@ class RatedView(MethodView):
     '''View Recipes'''
     def get(self):
         '''Recipes'''
-        user_email = session.get('email')
-        if not user_email:
-            return render_template('login.html')
-        user = users.find_one({"email": user_email})
-        rated_recipes = user['rated']
-        return render_template('rated.html', recipes=rated_recipes)
+        if 'email' in session:
+            user_email = session.get('email')
+            user = users.find_one({"email": user_email})
+            rated_recipes = user['rated']
+            return render_template('rated.html', recipes=rated_recipes)
+        return render_template('login.html')
 
     def post(self):
         '''Like'''
@@ -77,17 +77,17 @@ class RateView(MethodView):
     '''RateView'''
     def post(self):
         '''Set Rate'''
-        user_email = session.get('email')
-        if not user_email:
-            return render_template('login.html')
-        data = request.data
-        data = json.loads(data)
-        recipe = dbrecipes.find_one({'Title': data['recipe']})
-        rating = int(data['rating'])
-        rated_recipe = {'Ingredients': recipe['Ingredients'],
-                        'Instructions': recipe['Instructions'],
-                        'Image_Name': recipe['Image_Name'] + '.jpg',
-                        'Rating': rating}
-        users.update_one({'email': user_email}, {'$set': \
-            {f"rated.{recipe['Title']}": rated_recipe}})
-        return 'Success', 200
+        if 'email' in session:
+            user_email = session.get('email')
+            data = request.data
+            data = json.loads(data)
+            recipe = dbrecipes.find_one({'Title': data['recipe']})
+            rating = int(data['rating'])
+            rated_recipe = {'Ingredients': recipe['Ingredients'],
+                            'Instructions': recipe['Instructions'],
+                            'Image_Name': recipe['Image_Name'] + '.jpg',
+                            'Rating': rating}
+            users.update_one({'email': user_email}, {'$set': \
+                {f"rated.{recipe['Title']}": rated_recipe}})
+            return 'Success', 200
+        return render_template('login.html')
