be2fd9dee63478f70d31a3e2cbdb083aaf107f1c
Lev Lysyk <levlisik@gmail.com>
2024-03-31 14:43:10 +0300

    deleted console.log

--- static/show_liked.js
+++ static/show_liked.js
@@ -132,16 +132,6 @@ for (var i = 0; i < expandButtons.length; i ++) {
         recipeDescription.classList.add('recipe-description');
         split_instructions = liked_recipes[this.value][3].trim().split(/\n/);
         var ul = document.createElement("ul");
-        // for (var part of split_instructions) {
-        //     for (line of part.split(/\. |\.$; /)) {
-        //         if (/[A-Z]+/i.test(line) && !["F", "tsp", "tbsp", "Tbsp", "Tsp"].some((el) => line.endsWith(el + "."))) {
-        //             line = line.trim()
-        //             ul.innerHTML += `<li>${line.charAt(0).toUpperCase() + line.slice(1)}</li>`;
-        //         };
-        //     }
-        //     ul.innerHTML += "<p></p>";
-        // };
-        console.log(liked_recipes[this.value][3].trim())
         for (var part of split_instructions) {
             for (line of my_split(part, ".", ["°f", "tsp", "tbsp"])) {
                 if (/[A-Z]+/i.test(line)) {
